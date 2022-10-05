d7486a61739815b4de6e850a8640d1bf2a2230bd
LassiMik <112076377+LassiMik@users.noreply.github.com>
2022-10-05 07:49:23 +0300

    mirror copy
    mirror copy

--- bookstore/bookstore/src/main/java/com/example/bookstore/domain/BookRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.bookstore.domain;
-
-import java.util.List;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface BookRepository extends CrudRepository<Book, Long> {
-	List<Book> findByAuthor(String author);
-}
--- bookstore/src/main/java/com/example/bookstore/domain/BookRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.bookstore.domain;
-
-import java.util.List;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface BookRepository extends CrudRepository<Booke, Long> {
-	List<Booke> findByAuthor(String author);
-}
